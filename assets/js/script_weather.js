8409d04d5d10e5688619e80520e9419a267f5a38
Ruben Corver <rubencorver@live.nl>
2020-06-11 10:55:46 +0200

    Added function to add weather

--- assets/js/script_weather.js
+++ assets/js/script_weather.js
@@ -1,7 +1,99 @@
 let key = "39fb34d336ee1f54dae30cf62b425b85";
 let units = "metric"
+let names = [];
 let positions = [];
 const language = "nl";
+let searchTerm;
+let weathernames = JSON.parse(localStorage.getItem("names"));
+
+window.onload = function () {
+    console.log(weathernames);
+    weatherLoad(weathernames);
+}
+
+//Load previous added city's
+function weatherLoad(weathernames){
+    console.log(weathernames, "ASDSADADSAD");
+    if(weathernames != null){
+        weathernames.forEach(function (name){
+            searchTerm = `?q=${name}`;
+            getWeather(searchTerm);
+        })
+    }
+}
+
+//Weather API call
+function getWeather() {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?${searchTerm}&APPID=${key}&lang=${language}&units=${units}`)
+        .then(result => {
+            return result.json();
+        }).then(function (weather) {
+            console.log(weather);
+            addWeather(weather);
+        })
+        .catch(function () {
+
+        });
+}
+
+function addWeather(weather) {
+    if (names.includes(weather.name.toLowerCase())) {
+        let weatherTemp = document.getElementsByClassName("weather__temp " + weather.name);
+        for (let i = 0; i < weatherTemp.length; i++) {
+            weatherTemp[i].innerHTML = Math.round(weather.main.temp) + "&deg;";
+        }
+        let weatherDesc = document.getElementsByClassName("weather__description " + weather.name);
+        for (let i = 0; i < weatherDesc.length; i++) {
+            weatherDesc[i].innerHTML = "Windspeed: " + weather.wind.speed;
+        }
+        let weatherRain = document.getElementsByClassName("weather__rain " + weather.name);
+        for (let i = 0; i < weatherRain.length; i++) {
+            weatherRain[i].innerHTML = weather.weather[0].main;
+        }
+        console.log("weather update")
+    } else {
+        let location = document.createElement("article")
+        location.className = "weather__location";
+
+        let drag = document.createElement("div")
+        drag.className = "weather__drag " + weather.name;
+        drag.id = weather.name;
+        drag.setAttribute("draggable", "true");
+        drag.setAttribute("ondragstart", "drag(event)");
+        let left = document.createElement("div")
+        left.className = "weather__left " + weather.name;
+        let cityName = document.createElement("h3")
+        cityName.className = "weather__city " + weather.name;
+        let temp = document.createElement("p")
+        temp.className = "weather__temp " + weather.name;
+        let right = document.createElement("div")
+        right.className = "weather__right " + weather.name;
+        let rain = document.createElement("p")
+        rain.className = "weather__rain " + weather.name;
+        let wind = document.createElement("p")
+        wind.className = "weather__wind " + weather.name;
+
+        location.setAttribute("ondrop", "drop(event)");
+        location.setAttribute("ondragover", "allowDrop(event)");
+
+        cityName.innerHTML = weather.name;
+        wind.innerHTML = "Windspeed: " + weather.wind.speed;
+        temp.innerHTML = Math.round(weather.main.temp) + "&deg; ";
+        rain.innerHTML = weather.weather[0].main;
+
+        left.appendChild(temp);
+        left.appendChild(cityName);
+        right.appendChild(rain)
+        right.appendChild(wind)
+        drag.appendChild(left);
+        drag.appendChild(right);
+        location.appendChild(drag);
+
+        names.push(weather.name.toLowerCase());
+        document.querySelector(".weather__container").appendChild(location);
+        localStorage.setItem("cities", JSON.stringify(names));
+    }
+}
 
 //Current location
 if (navigator.geolocation) {
@@ -10,18 +102,6 @@ if (navigator.geolocation) {
 function currentPosition(position) {
     latitude = position.coords.latitude;
     longitude = position.coords.longitude;
-    getWeather();
-}
-
-//Weather API call
-function getWeather() {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${key}&lang=${language}&units=${units}`)
-        .then(result => {
-            return result.json();
-        }).then(function (data) {
-            console.log(data);
-        })
-        .catch(function () {
-
-        });
+    searchTerm = `lat=${latitude}&lon=${longitude}`;
+    getWeather(searchTerm);
 }
